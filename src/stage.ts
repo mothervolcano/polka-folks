d4bb6b61b3ce26231a8f1255c4c54165cf26fbfe
Eduardo Barbosa <edo@mothervolcano.com>
2023-11-18 13:16:32 +0000

    refactor UI/layout

---
+++ src/stage.ts
@@ -0,0 +1,110 @@
+import { Layer } from 'paper';
+import { paper } from './components/paperStage';
+
+import Nerd from './polka_modules/archetypes/nerd';
+import Punk from './polka_modules/archetypes/punk';
+import Baroque from './polka_modules/archetypes/baroque';
+
+import { markPoint } from './lib/topo/utils/helpers';
+
+const DEBUG_GREEN = '#10FF0C';
+const GUIDES = '#06E7EF';
+
+
+
+let view: any
+let layer: any
+let origin: any
+
+let polka: any
+
+
+export function reset() {
+
+    paper.project.clear();
+    view = paper.project.view
+    layer = new Layer()
+};
+
+
+export function generate(
+  
+  archetype: any,
+  baseParams: any,
+  archetypeParams: any,
+
+) {
+
+
+  origin = view.center;
+
+  console.log(`..... Generating Polka: `, archetype);
+
+  if ( archetype ) {
+
+    polka = new archetype( origin, 100 );
+
+  } else {
+
+    //TODO: handle instance management
+  }
+
+
+  polka.generate( { baseParams, archetypeParams } );
+
+}
+
+// ------------------------------------------------
+
+export function model(
+
+  baseParams: any,
+  archetypeParams: any,
+
+) {
+
+  origin = view.center;
+
+  polka.model( { baseParams, archetypeParams } );
+
+}
+
+// ------------------------------------------------
+
+export function regenerate(
+
+  baseParams: any,
+  archetypeParams: any,
+
+) {
+
+  origin = view.center;
+
+  console.log(`..... Regenerating Polka`);
+
+  polka.clear();
+  polka.generate( { baseParams, archetypeParams } );
+
+
+}
+
+// ------------------------------------------------
+
+export function save() {
+
+  const svg = paper.project.exportSVG({ bounds:'content', asString:true }) as string;
+
+
+  const svgFile = new Blob([svg], { type: 'image/svg+xml' });
+
+  const url = URL.createObjectURL(svgFile);
+
+  const dwnLink = document.createElement('a');
+  dwnLink.href = url;
+  dwnLink.download = 'artwork.svg'; 
+  dwnLink.click();
+  URL.revokeObjectURL(url);
+
+}
+
+
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,357 +1,316 @@
+import { useMediaQuery } from "react-responsive";
 
-import { useMediaQuery } from 'react-responsive';
+import { useState, useEffect } from "react";
 
-import { useState, useEffect } from 'react';
+import Stage from "./components/stage";
+import Button from "./components/ui/button";
 
-import Stage from './components/stage';
-import Button from './components/ui/button';
-import NerdConsole from './components/consoles/nerdConsole';
-import BaroqueConsole from './components/consoles/baroqueConsole';
-import PunkConsole from './components/consoles/punkConsole';
+import ArchetypeGenerationModal from "./components/modals/archetypeGenerationModal";
 
-import ArchetypeGenerationModal from './components/modals/archetypeGenerationModal';
+import { ParamSet, Param, Model } from "./types";
 
-import { reset, generate, regenerate, model, save } from './main';
+import useModel from "./hooks/useModel";
+import useArchetype from "./hooks/useArchetype";
 
+import { reset, generate, regenerate, model, save } from "./stage";
+import {
+	AspectRatio,
+	Container,
+	DEFAULT_THEME,
+	Grid,
+	Group,
+	Space,
+	Stack,
+	Title,
+	Text,
+} from "@mantine/core";
+import PaperStage from "./components/paperStage";
+
+// --------------------------------------------------------------
+// HELPERS
+
+function parseParams(updatedParams: ParamSet) {
+	const modelParams: any = {};
+
+	Array.from(updatedParams.values()).forEach((p: any) => {
+		modelParams[p.name] = p.value;
+	});
+
+	return modelParams;
+}
+
+// --------------------------------------------------------------
 
 const UI = () => {
+	const [isPaperLoaded, setIsPaperLoaded] = useState<boolean>(false);
+	const [initialized, setInitialized] = useState<boolean>(false);
+	const [models, currentModel, setCurrentModel] = useModel();
+	const [archetypes, currentArchetype, setCurrentArchetype] = useArchetype();
 
+	const [baseParams, setBaseParams] = useState<ParamSet | null>(null);
+	const [paramsForArchetype, setParamsForArchetype] =
+		useState<ParamSet | null>(null);
 
-	interface Param {
-				
-		id: string;
-		name: string;
-		value: number;
-		range: [number,number];
-		step: number;
-		label: string;
-	}
-
-	type ParamSet = Array<Param>;
-
-	interface Model {
-
-		option: string;
-		label: string;
-		console: any;
-		params: ParamSet;
-	}
-
-
-	const nerdParamsSchema: ParamSet = [
-
-		{ id:'ogp1', 	name: 'splitLat', 		value: 1, 		range: [0,2], 		step: 0.01, 	label: "Split Drop", },
-		{ id:'ogp2', 	name: 'splitAperture', 	value: 0.5,		range: [0,2], 		step: 0.01, 	label: "Split Aperture", },
-		{ id:'ogp3', 	name: '', 				value: 1, 		range: [0,2], 		step: 1, 	 	label: "Olga P3", },
-		{ id:'ogp4', 	name: '', 				value: 0.5, 	range: [0,2], 		step: 1, 	 	label: "Olga P4", },
-		{ id:'ogp5', 	name: '', 				value: 1, 		range: [0,2], 		step: 1, 	 	label: "Olga P5", },
-		{ id:'ogp6', 	name: '', 				value: 1, 		range: [0,2], 		step: 1, 	 	label: "Olga P6", },
-		{ id:'ogp7', 	name: '', 				value: 1, 		range: [0,2], 		step: 1, 	 	label: "Olga P7", },
-	]
-
-	const punkParamsSchema: ParamSet = [
-
-		{ id:'syp1', 	name: 'spikeNumCtrl',		value: 5, 		range: [1,15],		step: 1, 		label: "Spike Number", },
-		{ id:'syp5', 	name: 'shaveDotsDensity',	value: 5, 		range: [3,10],		step: 1, 		label: "Shaved Head", },
-		{ id:'syp2', 	name: 'spikeLengthCtrl',	value: 1,		range: [0,2],		step: 0.01, 	label: "Spike Height", },
-		{ id:'syp3', 	name: 'spikeSpreadCtrl',	value: 0.5, 	range: [0,1],		step: 0.01, 	label: "Spike Spread", },
-		{ id:'syp4', 	name: 'shrinkRateCtrl',		value: 0, 		range: [0,1],		step: 0.01, 	label: "Spike Shrink Rate", },
-		{ id:'syp6', 	name: 'spikeSharpnessCtrl',	value: 1, 		range: [0,2],		step: 0.01, 	label: "Spike Sharpness", },
-		{ id:'syp7', 	name: '',					value: 1, 		range: [0,2],		step: 0.01, 	label: "Syd P7", },
-	]
-
-	const baroqueParamsSchema: ParamSet = [
-
-		{ id:'mzp4', 	name: 'curlNumCtrl', 		value: 2, 		range: [1,6],		step: 1, 		label: "Curls", },
-		{ id:'mzp5', 	name: '', 					value: 1, 		range: [0,2],		step: 0.01, 	label: "Mozart P5", },
-		{ id:'mzp1', 	name: 'heightCtrl', 		value: 0.5, 	range: [0,1],		step: 0.01, 	label: "Height", },
-		{ id:'mzp7', 	name: 'volCtrl', 			value: 0.5, 	range: [0,1],		step: 0.01, 	label: "Volume", },
-		{ id:'mzp2', 	name: 'hairlineLevelCtrl', 	value: 0.5,		range: [0,2],		step: 0.01, 	label: "Hairline Level", },
-		{ id:'mzp3', 	name: 'hairlineRidgeCtrl', 	value: 1, 		range: [0,2],		step: 0.01, 	label: "Hairline Ridge", },
-		{ id:'mzp6', 	name: 'spanCtrl', 			value: 1, 		range: [0,2],		step: 0.01, 	label: "Span", },
-	]
-
-
-	const baseParamSchema: ParamSet = [
-
-		{ id:'hp1', 	name: '', 							value: 1, 		range: [0,2],		step: 0.01, 	label: "Ears Size", },
-		{ id:'hp2', 	name: '', 							value: 1, 		range: [0,2],		step: 0.01, 	label: "Ears Height", },
-		{ id:'hp3', 	name: '', 							value: 1, 		range: [0,2],		step: 0.01, 	label: "...", },
-		{ id:'eyep1', 	name: 'eyeScaleCtrl', 				value: 1, 		range: [0,2],		step: 0.01, 	label: "Eye Size", },
-		{ id:'eyep2', 	name: 'eyeDistanceCtrl', 			value: 1, 		range: [0,2],		step: 0.01, 	label: "Eye Distance", },
-		{ id:'eyep3', 	name: 'eyeRoundnessCtrl', 			value: 1, 		range: [0,2],		step: 0.01, 	label: "Eye Roundness", },
-		{ id:'eyep4', 	name: 'pTest', 						value: 1, 		range: [0,2],		step: 0.01, 	label: "p test 1", },
-		{ id:'eyep5', 	name: 'pTest2', 					value: 0, 		range: [-1,1],		step: 0.01, 	label: "p test 2", },
-		{ id:'nsp1', 	name: 'noseLengthCtrl', 			value: 1, 		range: [0,2],		step: 0.01, 	label: "Nose Length", },
-		{ id:'nsp2', 	name: 'noseScaleCtrl', 				value: 1, 		range: [0,2],		step: 0.01, 	label: "Nose Size", },
-		{ id:'nsp3', 	name: 'noseWidthCtrl', 				value: 0, 		range: [0,2],		step: 0.01, 	label: "Nose Width", },
-	]
-
-
-	const archetypes: any = [
-
-		{ option: "PUNK", 		label: "Punk", 		icon: "TEST", 	console: PunkConsole, 		params: punkParamsSchema, 			default: false, 	checked: false },
-		{ option: "BAROQUE", 	label: "Baroque", 	icon: "TEST", 	console: BaroqueConsole, 	params: baroqueParamsSchema, 		default: false, 	checked: false },
-		{ option: "NERD", 		label: "Nerd", 		icon: "TEST", 	console: NerdConsole, 		params: nerdParamsSchema, 			default: false, 	checked: false },
-	]
-
-
-	const [ isDesktopOrLaptop, setIsDesktopOrLaptop ] = useState(false);
-	const [ isPaperLoaded, setIsPaperLoaded ] = useState(false);
-
-	const [ initialized, setInitialized ] = useState(false);
-
-	const [ inArchetypeGenerationScreen, setInArchetypeGenerationScreen ] = useState<boolean>( true );
-
-	const [ baseParams, setBaseParams ] = useState< ParamSet >( baseParamSchema );
-
-	const [ currentArchetype, setCurrentArchetype ] = useState< Model | null >(null);
-	const [ paramsForArchetype, setParamsForArchetype ] = useState< ParamSet | null >( null );
-
-	const [ scaleCtrl, setScaleCtrl ] = useState(3);
-
-	const _isDesktopOrLaptop = useMediaQuery({
-    	
-    	query: '(min-width: 1224px)'
-  	});
-
+	const [scaleCtrl, setScaleCtrl] = useState(3);
 
 	//-----------------------------------------------------------------------
 
-	
-	useEffect( () => {
-
-		setIsDesktopOrLaptop( _isDesktopOrLaptop );
-
-	}, [] );
-
-
-	useEffect( () => { 
-
-		if ( isPaperLoaded ) {
-
-			// reset();
-			const _archetypeParams: any = {};
-			const _baseParams: any = {};
-
-			if ( currentArchetype === null ) {
-
-			} else {
-
-				Array.from(currentArchetype.params.values() || []).forEach( (p: any) => {
-
-					_archetypeParams[p.name] = p.value; 
-
-				});	
-			}
-
-			Array.from(baseParams?.values() || []).forEach( (p: any) => {
-
-				_baseParams[p.name] = p.value;
-			});
-
-			model( _baseParams, _archetypeParams  );
+	useEffect(() => {
+		if (!isPaperLoaded) {
+			console.log("PAPER ISN'T LOADED");
+			return () => {};
 		}
 
-	}, [ scaleCtrl, baseParams, paramsForArchetype ] );
+		console.log("1 --> PAPERJS LOADED! CurrentModel: ", currentArchetype);
 
+		const updatedBaseParams = parseParams(currentModel.params);
+		const updatedArchetypeParams = parseParams(currentArchetype.params);
+		setBaseParams(currentModel.params);
+		setParamsForArchetype(currentArchetype.params);
+
+		reset();
+		generate(
+			currentArchetype.model,
+			updatedBaseParams,
+			updatedArchetypeParams,
+		);
+		model(updatedBaseParams, updatedArchetypeParams);
+
+
+		if (!initialized) {
+			setInitialized(true);
+		}
+	}, [isPaperLoaded]);
+
+	useEffect(() => {
+		if (!isPaperLoaded) {
+			console.log("PAPER ISN'T LOADED");
+			return () => {};
+		}
+
+		console.log(
+			"1 --> SWITCH ARCHETYPE! currentArchetype: ",
+			currentArchetype,
+		);
+
+		const updatedBaseParams = parseParams(currentModel.params);
+		const updatedArchetypeParams = parseParams(currentArchetype.params);
+
+		reset();
+		generate(
+			currentArchetype.model,
+			updatedBaseParams,
+			updatedArchetypeParams,
+		);
+		model(updatedBaseParams, updatedArchetypeParams);
+	}, [currentArchetype]);
+
+	useEffect(() => {
+		if (!isPaperLoaded) {
+			console.log("PAPER ISN'T LOADED");
+			return () => {};
+		}
+
+		console.log(
+			"1 --> MODEL ARCHETYPE! currentArchetype: ",
+			currentArchetype,
+		);
+
+		const updatedBaseParams = parseParams(currentModel.params);
+		const updatedArchetypeParams = parseParams(currentArchetype.params);
+
+		model(updatedBaseParams, updatedArchetypeParams);
+	}, [paramsForArchetype]);
 
 	//-----------------------------------------------------------------------
-
-
-
-	function handleGenerateAction( selectedArchetype: any ) {
-
-		if ( isPaperLoaded ) {
-			
-			console.log(`ready to generate ${ selectedArchetype.label }`);
-
-			const _archetypeParams: any = {};
-			const _baseParams: any = {};
-
-			Array.from(selectedArchetype.params.values() || []).forEach( (p: any) => {
-
-				_archetypeParams[p.name] = p.value; 
-
-			});
-			
-			Array.from(baseParams.values() || []).forEach( (p: any) => {
-
-				_baseParams[p.name] = p.value;
-			});
-
-
-			reset();
-			generate( selectedArchetype.option, _baseParams, _archetypeParams  );
-			model( _baseParams, _archetypeParams );
-
-			setCurrentArchetype( selectedArchetype );
-			setParamsForArchetype( selectedArchetype.params );
-
-			setInArchetypeGenerationScreen( false );
-
-			if ( !initialized ) { setInitialized(true) };
-
-		} else {
-
-		}
-	};
-
 
 	function handleRegenerateAction() {
-
-		if ( isPaperLoaded && currentArchetype ) {
-			
-			console.log(`ready to regenerate ${ currentArchetype.label }`);
-
-			const _archetypeParams: any = {};
-			const _baseParams: any = {};
-
-			Array.from(currentArchetype?.params.values() || []).forEach( (p: any) => {
-
-				_archetypeParams[p.name] = p.value; 
-
-			});
-
-			Array.from(baseParams.values() || []).forEach( (p: any) => {
-
-				_baseParams[p.name] = p.value;
-			});
-
-			// reset();
-			regenerate( _baseParams, _archetypeParams );
-			model( _baseParams, _archetypeParams );
-
-		} else {
-
+		if (!isPaperLoaded) {
+			console.log("PAPER ISN'T LOADED");
+			return () => {};
 		}
-	};
 
+		console.log(`ready to regenerate ${currentArchetype.label}`);
 
-	function handleParamCtrlInputForArchetype( updatedParams: ParamSet ) {
+		const updatedBaseParams = parseParams(currentModel.params);
+		const updatedArchetypeParams = parseParams(currentArchetype.params);
 
-		setParamsForArchetype( updatedParams );
-	};	
-
-
-	// ----------------------------------------------
-	// UI HANDLERS
-
-	function handleNewArchetypeAction() {
-
-		setInArchetypeGenerationScreen( true );
+		// reset();
+		regenerate(updatedBaseParams, updatedArchetypeParams);
+		model(updatedBaseParams, updatedArchetypeParams);
 	}
 
-	function handleSaveAction() {
+	function handleParamCtrlInputForArchetype(updatedParams: ParamSet) {
+		setParamsForArchetype(updatedParams);
+	}
 
+	// ------------------------------------------------------------------------
+	// UI HANDLERS
+
+	function handleSaveAction() {
 		save();
 	}
 
+	// ------------------------------------------------------------------------
+	// BLOCKS
+
+	function switchConsole(archetype: Model) {
+		const ArchetypeConsole = archetype.console;
+
+		return (
+			<ArchetypeConsole
+				params={paramsForArchetype}
+				inputHandler={handleParamCtrlInputForArchetype}
+			/>
+		);
+	}
 
 	// ------------------------------------------------------------------------
 
-
-	function switchConsole( archetype: Model ) {
-
-		const ArchetypeConsole = archetype.console;
-
-		return ( <ArchetypeConsole  params={paramsForArchetype} inputHandler={handleParamCtrlInputForArchetype} /> )
-	}
-
+	const frameMargin = 6;
+	const dark = DEFAULT_THEME.colors.dark[5];
+	const softDark = DEFAULT_THEME.colors.dark[0];
+	const light = DEFAULT_THEME.colors.gray[0];
+	const softLight = DEFAULT_THEME.colors.gray[2];
 
 	return (
-        
-        <div className={`relative w-3/4 h-[80vh] m-5 border border-slate-900`}>
-		  	
-		  	<div className={`w-full h-full`}>
-
-				{ isDesktopOrLaptop && (
-
-                    <Stage
-
-                    	onPaperLoad={setIsPaperLoaded}
-                    />
-
-				)}
-
-		    </div>
-
-
-		    <div className={`absolute top-0 left-0 w-full h-full`}>
-        		
-        		{
-
-        			isDesktopOrLaptop && inArchetypeGenerationScreen && (
-
-        				<ArchetypeGenerationModal 
-
-        					initialized={ initialized }
-        					options={ archetypes }
-        					onGenerate={ handleGenerateAction }
-        					onClose={ () => setInArchetypeGenerationScreen(false) }
-        				/>
-
-        			)
-        		}
-
-        	</div>
-
-
-        	<div className={`absolute ${ isDesktopOrLaptop ? "top-0" : "top-0" } left-0 ${ isDesktopOrLaptop ? "max-w-[250px]" : "w-full" } ${ isDesktopOrLaptop ? "h-fit" : "h-[70vh]" } m-5 border border-slate-900`} > 
-
-
-		    	{
-		    		currentArchetype && !inArchetypeGenerationScreen && switchConsole( currentArchetype )
-		    	}
-
-		    </div>
-
-
-		    <div className={`absolute top-0 right-0 m-2`}>
-		    	
-		    	{
-
-		    		!inArchetypeGenerationScreen && (
-
-			    		<div>
-
-				    		<Button		
-								labelText="new"
-								onClickEventHandler={ handleNewArchetypeAction }
-							/>
-
-							<Button		
-								labelText="export"
-								onClickEventHandler={ handleSaveAction }
-							/>
-
-			    		</div>
-					)
-		    	}
-
-		    </div>
-
-
-		    <div className={`absolute bottom-0 right-0 m-2`}>
-
-		    	{
-
-		    		!inArchetypeGenerationScreen && currentArchetype && (
-
-						<Button	
-							labelText="regenerate"
-							onClickEventHandler={ handleRegenerateAction }
-						/>
-		    		)
-		    	}
-			
+		<div
+			style={{
+				position: "relative",
+				width: "100%",
+				height: "100vh",
+				padding: `${frameMargin}vh`,
+			}}
+		>
+			<div style={{ border: `1px solid ${dark}`, borderRadius: `10px` }}>
+				<Grid align="stretch" gutter={0}>
+					<Grid.Col span={2}>
+						<Container
+							fluid
+							w="100%"
+							bg={dark}
+							pt="sm"
+							pb="md"
+							mb="md"
+							style={{ borderRadius: "8px 0 0 0" }}
+						>
+							<Title c={light}>Polka Folks</Title>
+							<Space h="md" />
+							<Text size="sm" c={softLight}>
+								Project description goes here. It should be a
+								brief succint text introducing the concept
+							</Text>
+						</Container>
+						<Stack w={"100%"} p={15}>
+							{initialized &&
+								currentArchetype &&
+								switchConsole(currentArchetype)}
+						</Stack>
+					</Grid.Col>
+					<Grid.Col span={10}>
+						<div
+							style={{
+								position: "relative",
+								height: `${100 - frameMargin * 2}vh`,
+								borderLeft: `1px solid ${dark}`,
+							}}
+						>
+							<div
+								style={{
+									position: "absolute",
+									top: "15px",
+									left: "15px",
+								}}
+							>
+								<Stack gap={9}>
+									<Text
+										size="sm"
+										fw={500}
+										c={softDark}
+										ml="1vw"
+									>
+										Choose a model...
+									</Text>
+								</Stack>
+							</div>
+							<div
+								style={{
+									position: "absolute",
+									bottom: "0px",
+									left: "0px",
+									width: "100%",
+								}}
+							>
+								<Group grow gap={0}>
+									<div
+										style={{
+											borderRight: "1px solid black",
+											borderTop: "1px solid black",
+										}}
+									>
+										<AspectRatio ratio={1 / 1}>
+											<Container>slot 1</Container>
+										</AspectRatio>
+									</div>
+									<div
+										style={{
+											borderRight: "1px solid black",
+											borderTop: "1px solid black",
+										}}
+									>
+										<AspectRatio ratio={1 / 1}>
+											<Container>slot 2</Container>
+										</AspectRatio>
+									</div>
+									<div
+										style={{
+											borderRight: "1px solid black",
+											borderTop: "1px solid black",
+										}}
+									>
+										<AspectRatio ratio={1 / 1}>
+											<Container>slot 3</Container>
+										</AspectRatio>
+									</div>
+									<div
+										style={{
+											borderRight: "1px solid black",
+											borderTop: "1px solid black",
+										}}
+									>
+										<AspectRatio ratio={1 / 1}>
+											<Container>slot 4</Container>
+										</AspectRatio>
+									</div>
+									<div
+										style={{
+											borderRight: "1px solid black",
+											borderTop: "1px solid black",
+										}}
+									>
+										<AspectRatio ratio={1 / 1}>
+											<Container>slot 5</Container>
+										</AspectRatio>
+									</div>
+									<div
+										style={{
+											borderRight: "1px solid black",
+											borderTop: "1px solid black",
+										}}
+									>
+										<AspectRatio ratio={1 / 1}>
+											<Container>slot 6</Container>
+										</AspectRatio>
+									</div>
+								</Group>
+							</div>
+							<PaperStage onPaperLoad={setIsPaperLoaded} />
+						</div>
+					</Grid.Col>
+				</Grid>
 			</div>
-
 		</div>
-
-	)
+	);
 };
 
-
 export default UI;
-
