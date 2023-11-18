437a969b0bfa20041d00d9b2d0b73f703f6325a6
Eduardo Barbosa <edo@mothervolcano.com>
2023-11-18 13:15:08 +0000

    install mantine framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-
 import UI from './ui';
 import Lab from './lab';
-
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      {/*<UI/>*/}
-      <Lab/>
+      <header>
+      <UI/>
+      {/*<Lab/>*/}
       </header>
     </div>
   );
